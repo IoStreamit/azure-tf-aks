9cc998606b5e993c900e236121a696a876afef01
fridos <fridmahdi@gmail.com>
2021-01-23 20:13:35 +0100

    change provider ids

--- providers.tf
+++ providers.tf
@@ -6,8 +6,8 @@ provider "azurerm" {
 provider "kubernetes" {
   version                = "~> 1.11.1"
   load_config_file       = false
-  host                   = azurerm_kubernetes_cluster.iostream.kube_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.iostream.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.iostream.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.iostream.kube_config.0.cluster_ca_certificate)
+  host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
 }
