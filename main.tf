e7901b8709f5f84fd212d4a70d9b8483c48923fa
fridos <fridmahdi@gmail.com>
2021-01-23 21:31:04 +0100

    resolve id issues

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix                      = var.dns_prefix
   kubernetes_version              = var.kubernetes_version
   node_resource_group             = var.node_resource_group
-  api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
+  api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges 
   enable_pod_security_policy      = var.enable_pod_security_policy
  
 
